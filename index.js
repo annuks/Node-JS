1627635b117743e404f7ba734d4d5d2ffd949534
Annu Kumar Singh <annukumarsingh.aks@gmail.com>
2022-06-25 22:36:00 +0530

    Filling form dynamically

--- index.js
+++ index.js
@@ -2,20 +2,54 @@ const express=require ('express');
 const path= require('path');
 const port= 8000;
 const ejs= require('ejs');
+const { urlencoded } = require('express');
 const app= express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
+app.use(urlencoded());
+
+var contactList=[
+    {
+        name:"Ramesh",
+        phoneNo:"24687568"
+
+    },
+    {
+        name:"Tony Stark",
+        phoneNo:"359347690347"
+    },
+    {
+        name:"Suresh",
+        phoneNo:"87347690347"
+    }
+]
+
 
 app.get('/', function(req,res){
 
-    return res.render('home',{title:"My Contacts List"});
-// res.send('<h1>cool, it is running</h1>');
+    return res.render('home',{
+        title:"Contacts List",
+        contact_list:contactList
+    });
 });
 
 app.get('/practice',function(req,res)
 {
-    return res.render('practice',{title:"Dynamic"});
+    return res.render('practice',{title:"Dynamic",
+
+});
+});
+
+
+app.post('/create-contact',function(req,res){
+    // let newContact = {
+    //     name : req.body.name,
+    //     phoneNo : req.body.phoneNo
+    // }
+
+    contactList.push(req.body);
+    return res.redirect('/')
 });
 
 
